3789ac663bc802d39913130f745d488d814cdc56
Martin S <msilberg@brandeis.edu>
2015-03-19 15:37:27 -0400

    simple version

--- app.rb
+++ app.rb
@@ -4,42 +4,39 @@ require './models/user'
 require './models/follow'
 require 'sinatra/flash'
 
-##These are test users and a test relationship that are in the initial table. Get rid of them in the final implementation.
-User.delete_all
-Follow.delete_all
+##These are test users that are in the initial table. Get rid of them in the final implementation.
+#They are only added if these users haven't been added yet.
+if User.where(handle: "Test0 Follower").first == nil
+	user = User.new(handle: "Paul")
+	user.save
 
-user = User.new(handle: "Paul")
-user.save
-
-user = User.new(handle: "Carl")
-user.save
-		
-user = User.new(handle: "Test0 Follower")
-user.save
-		
-user = User.new(handle: "Test1 Follower")
-user.save
-		
-user = User.new(handle: "Test2 Follower")
-user.save
-		
-user = User.new(handle: "Test3 Follower")
-user.save
-
-#Paul follows Carl
-user1 = User.where(handle: "Paul").first
-user2 = User.where(handle: "Carl").first
-relation = Follow.new(follower_id: user1.id, followee_id: user2.id)
-relation.save
-
+	user = User.new(handle: "Carl")
+	user.save
+			
+	user = User.new(handle: "Test0 Follower")
+	user.save
+			
+	user = User.new(handle: "Test1 Follower")
+	user.save
+			
+	user = User.new(handle: "Test2 Follower")
+	user.save
+			
+	user = User.new(handle: "Test3 Follower")
+	user.save
+end
 
 get '/' do
+	@users = []
+	User.find_each do |user|
+		@users.push(user)
+	end
 	erb :index
 end
  
 post '/submit' do
 	rando = Random.rand(3)
-	user1 = User.where(handle: "Test#{rando.to_s} Follower").first#Replace with code that identifies the logged-in user
+	user1 = User.where(handle: "Test0 Follower").first#Replace with code that identifies the logged-in user
 	user2 = User.where(handle: params[:followee]).first
 	if user1 == nil || user2 == nil
 		"One of you doesn't exist."
@@ -59,7 +56,7 @@ end
 
 get '/followlists' do
 	@followers = Follow.return_followers(User.where(handle: "Carl"))
-	@followees = Follow.return_followees(User.where(handle: "Test2 Follower"))
+	@followees = Follow.return_followees(User.where(handle: "Test0 Follower"))
 	@relationships = Follow.return_relationships()
 	erb :followlists
 end
